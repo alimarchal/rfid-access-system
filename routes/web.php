315a3696fefd2845b475cdf8d5b43051f72073bb
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-01-28 10:13:33 +0500

    added vehicle routes

--- routes/web.php
+++ routes/web.php
@@ -20,22 +20,12 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',
     Route::get('/dashboard', function () { return view('dashboard');})->name('dashboard');
     Route::resource('/location', LocationController::class);
     Route::resource('/users', UserController::class);
-
     Route::resource('rfid-cards', RfidCardController::class);
     Route::post('rfid-cards/{rfidCard}/reassign', [RfidCardController::class, 'reassign'])->name('rfid-cards.reassign');
 
-
+    Route::resource('vehicles', VehicleController::class);
 });
 
-
-// Users
-//Route::resource('users', UserController::class);
-
-// RFID Cards
-
-
-// Vehicles
-Route::resource('vehicles', VehicleController::class);
 
 // Entries
 Route::resource('entries', EntryController::class)->only(['index', 'show']);
@@ -43,8 +33,6 @@ Route::resource('entries', EntryController::class)->only(['index', 'show']);
 // Family Members
 Route::resource('users.family-members', FamilyMemberController::class)->shallow();
 
-// Locations
-Route::resource('locations', LocationController::class);
 
 // Assignment History
 Route::resource('assignment-histories', AssignmentHistoryController::class)->only(['index', 'show']);
