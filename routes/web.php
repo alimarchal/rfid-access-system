4bce50b97665e59f51ee174fe98da3c4a311a3a3
Ayesha Ashraf <akh50381@gmail.com>
2025-02-06 17:34:31 +0500

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', function () { return view('dashboard');})->name('dashboard');
-
+   
 
     Route::resource('/administration/locations', LocationController::class);
     Route::resource('/administration/users', UserController::class);
